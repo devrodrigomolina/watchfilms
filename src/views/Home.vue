2b3bbfb3e86325e9ce9bc9fae5524e8999dec5ff
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-04-06 22:03:02 -0300

    Começando a implementação de seletor de filmes populares/mais acessados

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,10 +19,17 @@
         </div>
       </div>
     </div>
-
-    <h1 class="title-popular">Os mais populares</h1>
+    <div class="selects">
+      <h1 class="title-popular">Os mais populares</h1>
+      <ul>
+        <li @click="showPopulars()">Populares</li>
+        <li @click="showEvaluated()">Mais Avaliados</li>
+        <li>Para Alugar</li>
+        <li>Nos Cinemas</li>
+      </ul>
+    </div>
     <div class="popular-filmes">
-      <div class="cards-films" v-for="result2 in recomendedFilms.results" :key="result2.id">
+      <div class="cards-films" v-for="result2 in this.a.evaluatedFilms" :key="result2.id"> <!-- MEXER AQUI NESSA PORRA =D -->
         <img @click="getInfosFilm(result2.id)" :src="`http://image.tmdb.org/t/p/w500/${result2.poster_path}`" alt="">
         <p>{{result2.original_title}}</p>
       </div>
@@ -34,15 +41,17 @@
 import axios from 'axios'
 
 export default {
-
   data() {
     return {
       query: '',
       results: '',
       filmes: null,
-      recomendedFilms: '',
+      a: {popularsFilms: null, evaluatedFilms: null,},
       id: '',
     }
+  },
+  watch: {
+
   },
   methods: {
     searchFilm(query) {
@@ -54,10 +63,16 @@ export default {
     getInfosFilm(id) {
       this.$router.push({ name: 'details', params: { id } })
     },
+    showEvaluated() {
+      axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=e161437dd0afeb088fc7bc77be4d32bc&language=en-US&page=1')
+      .then(response => {
+        this.a.evaluatedFilms = response.data.results
+      })
+    },
     showPopulars() {
       axios.get('https://api.themoviedb.org/3/movie/popular?api_key=e161437dd0afeb088fc7bc77be4d32bc&language=en-US&page=1')
       .then(response => {
-        this.recomendedFilms = response.data
+        this.a.popularsFilms = response.data.results
       })
     },
   },
